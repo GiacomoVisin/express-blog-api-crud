c70a732123a3a3ccc3022d391c93f0718c9f0120
GiacomoVisin <killerboss1150@gmail.com>
2026-06-06 21:25:05 +0200

    adding the id

--- data/data.js
+++ data/data.js
@@ -1,34 +1,41 @@
 const array = [
     {
+        id: 1,
         titolo: "ciambellone",
         contenuto: "ciambellone ripieno",
         immagine: "images/ciambellone.jpeg",
-        tags: ["ciambellone","dolce","fatto in casa"]
+        tags: ["ciambellone", "dolce", "fatto in casa"]
     },
     {
+        id: 2,
         titolo: "cracker_barbabietola",
         contenuto: "cracker fatto a barbabietola",
         immagine: "images/cracker_barbabietola.jpeg",
-        tags: ["cracker","barbabietola","fatto in casa"]
+        tags: ["cracker", "barbabietola", "fatto in casa"]
     },
     {
+        id: 3,
         titolo: "pane_fritto_dolce",
         contenuto: "pane prima fritto poi dolce",
         immagine: "images/pane_fritto_dolce.jpeg",
-        tags: ["pane","dolce","fritto","fatto in casa"]
+        tags: ["pane", "dolce", "fritto", "fatto in casa"]
     },
     {
+        id: 4,
         titolo: "pasta_barbabietola",
         contenuto: "pasta alla barbabietola",
         immagine: "images/pasta_barbabietola.jpeg",
-        tags: ["pasta ","barbabietola","fatto in casa"]
+        tags: ["pasta ", "barbabietola", "fatto in casa"]
     },
     {
+        id: 5,
         titolo: "torta_paesana",
         contenuto: "torta paesana al cioccolato",
         immagine: "images/torta_paesana.jpeg",
-        tags: ["torta","dolce","paesano","fatto in casa"]
+        tags: ["torta", "dolce", "paesano", "fatto in casa"]
     },
 ]
 
+
+    
 module.exports = array
