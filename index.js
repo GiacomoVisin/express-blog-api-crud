85035ee28dcc4ce15783582e8f8b26f12cb2cf75
GiacomoVisin <killerboss1150@gmail.com>
2026-06-06 12:20:07 +0200

    start server and log listening port

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const express = require(`express`)
 const app = express()
 const port = 3000
+
+app.listen(port,()=>{
+    console.log(`the app is workin on the port http://localhost: ${port}`);
+})
